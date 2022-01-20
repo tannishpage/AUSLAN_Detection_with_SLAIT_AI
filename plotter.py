9213b01cff2b9cfd2b997e114b16bca8e9d7a936
Tannishpage <tannishpage1@gmail.com>
2022-01-21 09:15:51 +1000

    Adding plot script

---
+++ plotter.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+import sys
+
+if __name__ == "__main__":
+"""    fig = plt.figure(1)
+    fig.set_size_inches((19.2, 10.8))
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(legend)
+    if plot_loc != "":
+        fig.savefig(plot_loc, dpi=100)
+    plt.show()"""
+    print("Not implemented")
+    exit(1)
